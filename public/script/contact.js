fe1a9a4b4d0eef2dac237d6d453e9f297b9ab1a3
GabrielVictor07 <bielmottagit@gmail.com>
2025-05-09 16:06:25 -0300

    ajuste

--- public/script/contact.js
+++ public/script/contact.js
@@ -7,7 +7,9 @@ window.onload = function () {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
       btn.disabled = true;
-      btn.innerText = "Enviando…";
+      let loader = document.querySelector(".loader")
+      loader.style.display = "flex"
+      btn.innerHTML = "Enviando..."
   
       console.log("► Tentando enviar formulário", {
         service: "service_9wwgq9p",
@@ -19,18 +21,24 @@ window.onload = function () {
         .then((result) => {
           console.log("✅ EmailJS result:", result);
           let msg = document.querySelector(".mensagem-text")
-          
-          msg.textContent = "Enviado com sucesso"
+          msg.style.display = "flex"     
+          msg.innerText = "Mensagem enviada com Sucesso!"
+          setTimeout(() => {
+            msg.style.display = "none";
+          }, 3000);
+          console.log(msg)
           form.reset();
         })
         .catch((error) => {
           console.error("🚨 EmailJS error:", error);
-          alert("Erro ao enviar. Veja console para detalhes.");
+          let msg = document.querySelector(".mensagem-text")
+          msg.style.display = "flex";     
+          msg.innerText = "Erro ao enviar Mensagem! Tente Novamente" 
         })
         .finally(() => {
           btn.disabled = false;
           btn.innerText = "Enviar";
+          loader.style.display = "none"
         });
     });
   };
-  
