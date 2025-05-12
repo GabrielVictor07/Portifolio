8973a450590fa733bd61f1a98ae2b4882cbd1db2
GabrielVictor07 <bielmottagit@gmail.com>
2025-05-12 11:58:33 -0300

    ajuste

--- public/script/script.js
+++ public/script/script.js
@@ -2,3 +2,19 @@ AOS.init({
         duration: 1000, // Tempo da animação (1s)
         once: false, // A animação ocorre varias vezes
     });
+
+
+function Navbar(){
+    let nav = document.querySelector('.navbar')
+    let navbutton = document.querySelector('.enter-nav-mobile')
+    nav.style.display = "flex"
+    navbutton.style.display = 'none'
+
+}
+
+function NavbarBack(){
+    let nav = document.querySelector('.navbar')
+    let navbutton = document.querySelector('.enter-nav-mobile')
+    nav.style.display = "none"
+    navbutton.style.display = 'flex'
+}
